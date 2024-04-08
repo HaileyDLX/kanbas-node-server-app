40fe8fc78848c428e353d58d8c35a09541eb40e8
HaileyDLX <15663006222@163.com>
2024-04-08 01:47:52 -0400

    a6 Mongo su23

--- App.js
+++ App.js
@@ -5,7 +5,10 @@ import cors from "cors";
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import AssignmentRoutes from "./Kanbas/assignments/routes.js";
+import mongoose from 'mongoose';
+import UserRoutes from "./kanbas-node-server-app/Users/routes.js";
 
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -16,4 +19,5 @@ AssignmentRoutes(app);
 
 Lab5(app);
 Hello(app);
+UserRoutes(app);
 app.listen( process.env.PORT || 4000);
